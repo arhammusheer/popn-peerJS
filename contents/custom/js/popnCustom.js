2e89395906553ace594209c73924741a31ff34cf
arhammusheer <arhammusheer@gmail.com>
2020-03-29 03:23:42 +0530

    new shit

--- contents/custom/js/popnCustom.js
+++ contents/custom/js/popnCustom.js
@@ -5,3 +5,22 @@ new titlebar.Titlebar({
     enableMnemonics	: false,
 });
 
+function init(){
+    var canvas = document.getElementsById('canvas'),
+    context = canvas.getContect('2d'),
+    video = document.getElementsById('video'),
+    vendorUrl = window.URL || window.webkitURL;
+
+    navigator.getMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUSerMedia ;
+    
+    navigator.getMedia({
+        video:true,
+        audio: false,
+    },function(stream){
+        video.src = vendorUrl.createObjectURL(stream);
+        video.play();
+    }, function(error){
+       //An error occured 
+    })
+};
